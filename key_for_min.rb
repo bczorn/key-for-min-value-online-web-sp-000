0ac71966ea7a18fdb0e8fa962cb6c466f75b9573
Becky Zorn <bczorn@gmail.com>
2019-09-05 02:05:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,11 @@
 def key_for_min_value(name_hash)
   array = name_hash.flatten
   if array[1] < array[3] && array[1] < array[5]
-    return 
+    return array[1]
+  elsif array[3] < array[1] && array[3] < array[5]
+    return array[3]
+  elsif array[5] < array[1] && array[5] < array[3]
+    return array[5]
+  else
+    nil
 end
