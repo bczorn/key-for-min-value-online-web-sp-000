fff6b4438e0d71d336f46eb80ddfbb5b41031679
Becky Zorn <bczorn@gmail.com>
2019-09-05 02:01:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  array = 
-    
+  array = name_hash.flatten
+  if array[1] < array[3] && array[1] < array[5]
+    return 
 end
